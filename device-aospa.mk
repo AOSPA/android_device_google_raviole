15562e6ca6c91bfebf3216adb20541b20a0e30ae
Chris Crump <chriscrawford893@gmail.com>
2022-09-13 18:31:55 -0400

    raviole: Update required vendor libraries
    Change-Id: I23d9a6a38ccb004352b072af926cbb7b5844ee18

--- device-aospa.mk
+++ device-aospa.mk
@@ -17,16 +17,64 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor:64 \
+    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor:64 \
+    hardware.google.bluetooth.ccc@1.0.vendor:64 \
+    hardware.google.bluetooth.sar@1.0.vendor:64 \
+    hardware.google.bluetooth.sar@1.1.vendor:64
+
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+# Codec2
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0.vendor \
+    libavservices_minijail.vendor:32 \
+    libavservices_minijail.vendor:64 \
+    libcodec2_hidl@1.0.vendor \
+    libcodec2_vndk.vendor \
+    libmedia_ecoservice.vendor \
+    libstagefright_bufferpool@2.0.1.vendor
+
+# Confirmation UI
+PRODUCT_PACKAGES += \
+    android.hardware.confirmationui@1.0-lib.trusty:64 \
+    android.hardware.confirmationui@1.0.vendor:64 \
+    libteeui_hal_support.vendor:64
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor
+
+# Identity credential
+PRODUCT_PACKAGES += \
+    android.hardware.identity-support-lib.vendor:64 \
+    android.hardware.identity_credential.xml
+
 # Kernel
 TARGET_PREBUILT_KERNEL := device/google/raviole-kernel/Image.lz4
+
+# Nos
+PRODUCT_PACKAGES += \
+    libnos:64 \
+    libnosprotos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnos_transport:64 \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64
 
 # Properties
 TARGET_VENDOR_PROP := device/google/raviole/vendor.prop
@@ -41,67 +89,53 @@ PRODUCT_PACKAGES += \
     ImsServiceEntitlement \
     Iwlan
 
-# Vendor Libraries
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.singlereg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.singlereg.xml
+
+# Tinycompress
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0.vendor:64 \
+    libtinycompress
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    libwifi-hal:64
+
+# Misc interfaces
+PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor:64 \
+    android.hardware.biometrics.common-V1-ndk_platform.vendor:64 \
     android.hardware.biometrics.fingerprint-V1-ndk_platform.vendor:64 \
-    android.hardware.bluetooth@1.1.vendor:64 \
-    android.hardware.confirmationui@1.0.vendor:64 \
-    android.hardware.confirmationui@1.0-lib.trusty:64 \
-    android.hardware.drm@1.4.vendor:64 \
-    android.hardware.keymaster@4.1.vendor:64 \
-    android.hardware.oemlock@1.0.vendor:64 \
-    android.hardware.identity_credential.xml \
-    android.hardware.identity-support-lib.vendor:64 \
     android.hardware.input.classifier@1.0.vendor:64 \
+    android.hardware.input.common@1.0.vendor:64 \
+    android.hardware.keymaster@3.0.vendor:64 \
+    android.hardware.keymaster@4.1.vendor:64 \
     android.hardware.neuralnetworks-V1-ndk_platform.vendor:64 \
-    android.hardware.neuralnetworks@1.3.vendor:64 \
+    android.hardware.oemlock@1.0.vendor:64 \
     android.hardware.power@1.2.vendor:64 \
-    android.hardware.radio@1.6.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.radio@1.6.vendor \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
     android.hardware.sensors@2.1.vendor \
     android.hardware.tetheroffload.config@1.0.vendor:64 \
     android.hardware.tetheroffload.control@1.1.vendor:64 \
     android.hardware.weaver@1.0.vendor:64 \
-    android.hardware.wifi@1.5.vendor:64 \
-    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor:64 \
-    hardware.google.bluetooth.ccc@1.0.vendor:64 \
-    hardware.google.bluetooth.sar@1.1.vendor:64 \
-    pixelpowerstats_provider_aidl_interface-cpp.vendor:64 \
-    libaudioroutev2.vendor \
-    libavservices_minijail_vendor:64 \
-    libcodec2_hidl@1.0.vendor \
-    libcodec2_vndk.vendor \
-    libcppbor.vendor:64 \
+    android.hardware.wifi@1.5.vendor:64
+
+# Misc
+PRODUCT_PACKAGES += \
     libGralloc4Wrapper \
-    libexynosv4l2 \
+    libaudioroutev2.vendor \
     libexynosutils \
-    libhwbinder.vendor \
-    libmedia_ecoservice.vendor \
+    libexynosv4l2 \
+    libsfplugin_ccodec_utils.vendor \
+    libcppbor.vendor:64 \
     libkeymaster4support.vendor:64 \
     libnetfilter_conntrack:64 \
     libnfnetlink:64 \
-    libnos:64 \
-    libnos_client_citadel:64 \
-    libnos_datagram:64 \
-    libnos_datagram_citadel:64 \
     libsensorndkbridge \
-    libsfplugin_ccodec_utils.vendor \
-    libtinycompress \
     libtrusty_metrics:64 \
-    libwifi-hal:64 \
-    nos_app_avb:64 \
-    nos_app_identity:64 \
-    nos_app_keymaster:64 \
-    nos_app_weaver:64 \
-    sensors.dynamic_sensor_hal \
-    vendor.google.google_battery@1.1.vendor:64 \
-    vendor.google.wireless_charger@1.3.vendor:64
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.singlereg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.singlereg.xml
+    pixelpowerstats_provider_aidl_interface-cpp.vendor:64 \
+    sensors.dynamic_sensor_hal
